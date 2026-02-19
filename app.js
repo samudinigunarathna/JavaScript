b70401c3f07142c8b59d959f70004bdc572e2f47
Samudini Gunarathna <samudinigunarathna@gmail.com>
2026-02-19 20:31:50 +0530

    17-02-2026

---
+++ app.js
@@ -0,0 +1,2 @@
+ console.log("Welcome to Web Servers and Technologies!");
+ console.log("Hello world!");
---
+++ operation.js
@@ -0,0 +1,24 @@
+var a = 8;
+var b = 7;
+
+//var c = a + b;
+console.log("Adding a and b equals to: " + (a + b));
+
+var d = a - b;
+console.log("Substituting a and b equals to: " + d);
+
+var e = a * b;
+console.log("Multiplying a and b equals to: " + e);
+
+var f = a/b;
+console.log("Dividing a by b equals to: " + f);
+
+var g = a%b;
+console.log("Modulation of a by b equals to: " + g);
+
+if(a > b){
+    console.log(a + " is greater than " + b);
+}
+else{
+    console.log(a + " is lower than " + b);
+}
